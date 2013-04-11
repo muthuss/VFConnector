5104586314d8396007b43e4554bd9fbd3cded37e
Muthu <muthu@softsquare.biz>
2013-04-11 22:46:34 +0530

    c4

--- assets/www/bootconfig.js
+++ assets/www/bootconfig.js
@@ -1,5 +1,3 @@
-     
-     
 //-----------------------------------------------------------------
 // Replace the values below with your own app configuration values.
 //-----------------------------------------------------------------
@@ -10,26 +8,25 @@ var debugMode = true;
 
 // The client ID value specified for your remote access object that defines
 // your application in Salesforce.
-var remoteAccessConsumerKey = "3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa";
+var remoteAccessConsumerKey = "___VARIABLE_publicKey___";
 
 // The redirect URI value specified for your remote access object that defines
 // your application in Salesforce.
-var oauthRedirectURI = "testsfdc:///mobilesdk/detect/oauth/done";
+var oauthRedirectURI = "___VARIABLE_redirectURL___";
 
 // The authorization/access scope(s) you wish to define for your application.
-var oauthScopes = ["web", "api"];
+var oauthScopes = ["web","api"];
 
-//The start data associated with the application.  Use SFHybridApp.LocalAppStartData for a "local"
-//PhoneGap-based application, and SFHybridApp.RemoteAppStartData for a Visualforce-based
-//application.  The default representations are below, or you can look at the data
-//classes in SFHybridApp.js to see how you can further customize your options.
-//var startData = new SFHybridApp.LocalAppStartData();  // Used for local REST-based "index.html" PhoneGap apps.
-var startData = new SFHybridApp.RemoteAppStartData("/apex/MobilePage"); // Used for Visualforce-based apps.
+// The start data associated with the application.  Use SFHybridApp.LocalAppStartData for a "local"
+// Cordova-based application, and SFHybridApp.RemoteAppStartData for a Visualforce-based
+// application.  The default representations are below, or you can look at the data
+// classes in SFHybridApp.js to see how you can further customize your options.
+var startData = new SFHybridApp.LocalAppStartData();  // Used for local REST-based "index.html" Cordova apps.
+//var startData = new SFHybridApp.RemoteAppStartData("/apex/BasicVFPage"); // Used for Visualforce-based apps.
 
+// At launch, should the app attempt to load previously cached content, when offline?
+var attemptOfflineLoad = true;
 
 //-----------------------------------------------------------------
 // End configuration block
 //-----------------------------------------------------------------
-        
-            
-
