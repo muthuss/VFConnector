0bdcfed59d33eb9942aa2469908badc8afebf8f1
Muthu <muthu@softsquare.biz>
2013-04-11 22:52:43 +0530

    c5

--- assets/www/bootconfig.js
+++ assets/www/bootconfig.js
@@ -8,11 +8,11 @@ var debugMode = true;
 
 // The client ID value specified for your remote access object that defines
 // your application in Salesforce.
-var remoteAccessConsumerKey = "___VARIABLE_publicKey___";
+var remoteAccessConsumerKey = "3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa";
 
 // The redirect URI value specified for your remote access object that defines
 // your application in Salesforce.
-var oauthRedirectURI = "___VARIABLE_redirectURL___";
+var oauthRedirectURI = "testsfdc:///mobilesdk/detect/oauth/done";
 
 // The authorization/access scope(s) you wish to define for your application.
 var oauthScopes = ["web","api"];
@@ -21,7 +21,7 @@ var oauthScopes = ["web","api"];
 // Cordova-based application, and SFHybridApp.RemoteAppStartData for a Visualforce-based
 // application.  The default representations are below, or you can look at the data
 // classes in SFHybridApp.js to see how you can further customize your options.
-var startData = new SFHybridApp.LocalAppStartData();  // Used for local REST-based "index.html" Cordova apps.
+var startData = new SFHybridApp.LocalAppStartData("/apex/MobilePage");  // Used for local REST-based "index.html" Cordova apps.
 //var startData = new SFHybridApp.RemoteAppStartData("/apex/BasicVFPage"); // Used for Visualforce-based apps.
 
 // At launch, should the app attempt to load previously cached content, when offline?
